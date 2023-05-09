046c53b5bbbf17d25f98822bae9a7ac6fa52089f
shekhar <chanduwadibhasme@gmail.com>
2023-05-09 19:03:40 +0530

    Prime-number program submitted

---
+++ primenumber.java
@@ -0,0 +1,9 @@
+class Primenumber{
+    public static void main(String arg[]){
+        int a []={2,3,5,7,11,13,17,19};
+        for(int i=0;i<a.length;i++)
+        {
+       System.out.println("List of prime number "+a[i]);
+    }
+    }
+}
